8920839ef9dceb91ae0546c17cbb81a37d635815
Jackson <xzxiong@yeah.net>
2024-06-21 19:56:13 +0800

    testcase: stackRefGetStringOutput / stackRefGetOutput

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"path"
+
 	//appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apps/v1"
 	//corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
 	//metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
@@ -42,6 +44,68 @@ func main() {
 		// ctx.Export("name", deployment.Metadata.Name())
 		ctx.Export("author", pulumi.String(cfg.Require("author")))
 
+		funcName := cfg.Get("get-function-name")
+		if funcName == "" || funcName == "stackRefGetOutput" {
+			if err := stackRefGetOutput(ctx, cfg); err != nil {
+				return err
+			}
+		} else {
+			if err := stackRefGetStringOutput(ctx, cfg); err != nil {
+				return err
+			}
+		}
+
 		return nil
 	})
+}
+
+func stackRefGetStringOutput(ctx *pulumi.Context, cfg *config.Config) error {
+
+	ctx.Log.Info("run stackRefGetStringOutput(not-exist-key) will get error", nil)
+
+	dstStack := "dev"
+	if ctx.Stack() == "dev" {
+		dstStack = "qa"
+	}
+
+	stack := path.Join(ctx.Organization(), ctx.Project(), dstStack)
+	s, err := pulumi.NewStackReference(ctx, stack, nil)
+	if err != nil {
+		return err
+	}
+
+	output := s.GetStringOutput(pulumi.String("not-exist-key")).ApplyT(func(v string) string {
+		return v
+	})
+	ctx.Export("output", output)
+	return nil
+}
+
+func stackRefGetOutput(ctx *pulumi.Context, cfg *config.Config) error {
+
+	ctx.Log.Info("run stackRefGetOutput(not-exist-key) can run ok", nil)
+
+	dstStack := "dev"
+	if ctx.Stack() == "dev" {
+		dstStack = "qa"
+	}
+
+	stack := path.Join(ctx.Organization(), ctx.Project(), dstStack)
+	s, err := pulumi.NewStackReference(ctx, stack, nil)
+	if err != nil {
+		return err
+	}
+
+	output := s.GetOutput(pulumi.String("not-exist-key")).ApplyT(func(v interface{}) string {
+		if v == nil {
+			return "not-exist"
+		}
+		s, ok := v.(string)
+		if !ok {
+			return "not-string-val"
+		}
+		return s
+	})
+	ctx.Export("output", output)
+	return nil
 }
